b55d8b54b855d32e4c6f3fff352723af2883f185
Andre Natal <anatal@gmail.com>
2017-04-17 20:13:28 -0700

    Using emscripten heap to communicate with c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include "webrtc/common_audio/vad/include/webrtc_vad.h"
 #include "webrtc/common_audio/signal_processing/include/signal_processing_library.h"
@@ -16,10 +17,35 @@ int main() {
     if (ret_state == -1) return 0;
     ret_state = WebRtcVad_Init(handle);
     if (ret_state == -1) return 0;
-    ret_state = WebRtcVad_set_mode(handle, 3);
-    if (ret_state == -1) return 0;
     printf("Webrtc_vad started \n");
 
-    //int level = WebRtcVad_Process(handle, 16000, data, n_samples);
     return 1;
+}
+
+int setmode(int mode){
+    printf("mode set to %i \n", mode);
+    if (!handle) {
+        printf("not handle \n");
+    }
+    return WebRtcVad_set_mode(handle, mode);
+}
+
+int process_data(int16_t  data[], int n_samples, int samplerate, int val0, int val100, int val2000){
+    printf("pointer here %p\n", (void *) &data);
+    printf("n_samples here %i\n", n_samples);
+    printf("samplerate here %i\n", samplerate);
+    printf("val0 here %i\n", val0);
+    printf("data[0] aqui %i \n", data[0]);
+
+    if (data == NULL) {
+        printf("data == NULL \n");
+    }
+
+    //for (int i = 0; i <= 2048; i++) {
+    assert(data[0] == val0);
+    assert(data[100] == val100);
+    assert(data[2000] == val2000);
+
+    //return WebRtcVad_Process(handle, samplerate, data, n_samples);
+    return 99;
 }
